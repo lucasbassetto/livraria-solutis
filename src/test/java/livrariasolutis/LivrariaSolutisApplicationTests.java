1c5b82562d5ad7ab2ec828bb5901a9deb57d5659
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-08-13 19:48:59 -0300

    Preparação ambiente, classe Livro

---
+++ src/test/java/livrariasolutis/LivrariaSolutisApplicationTests.java
@@ -0,0 +1,13 @@
+package livrariasolutis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaSolutisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
